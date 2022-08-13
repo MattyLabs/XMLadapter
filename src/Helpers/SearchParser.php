fae5044cf97e7eadc8c8295370203ac80f536566
Matthew Pollock <mattyp@pollocks.london>
2022-08-13 08:01:48 +0100

    Update SearchParser.php

--- src/Helpers/SearchParser.php
+++ src/Helpers/SearchParser.php
@@ -413,6 +413,7 @@
                     $val = str_replace(">", '&gt;', $val);
                     $val = str_replace("<=", '&lte;', $val);
                     $val = str_replace(">=", '&gte;', $val);
+		    $val = str_replace("{63}", '?', $val);
 
                     if(substr_count($val, '"') % 2 != 0){
                         // if there are an odd number of quotes now, get rid of them all!
