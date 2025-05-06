918efc6c1deb6e3c7cd24dad215274702bead3de
MattyP <mattyp@pollocks.london>
2025-05-06 11:23:44 +0100

    v3.0.10
    Bugfix titeCase() function - remove multibyte handling.

--- src/Helpers/HelperFunctions.php
+++ src/Helpers/HelperFunctions.php
@@ -469,7 +469,6 @@
          */
         public static function titleCase($title)
         {
-
             //remove HTML, storing it for later
             //       HTML elements to ignore    | tags  | entities
             $regx = '/<(code|var)[^>]*>.*?<\/\1>|<[^>]+>|&\S+;/';
@@ -477,50 +476,47 @@
             $title = preg_replace ($regx, '', $title);
             
             //find each word (including punctuation attached)
-            preg_match_all ('/[\w\p{L}&`\'‘’"“\.@:\/\{\(\[<>_]+-? */u', $title, $m1, PREG_OFFSET_CAPTURE);
+			preg_match_all ('/[\w\p{L}&`\'‘’"“\.@:\/\{\(\[<>_]+-? */', $title, $m1, PREG_OFFSET_CAPTURE);
             foreach ($m1[0] as &$m2) {
                 //shorthand these- "match" and "index"
                 list ($m, $i) = $m2;
                 
                 //correct offsets for multi-byte characters (`PREG_OFFSET_CAPTURE` returns *byte*-offset)
                 //we fix this by recounting the text before the offset using multi-byte aware `strlen`
-                $i = mb_strlen (substr ($title, 0, $i), 'UTF-8');
+				$i = strlen (substr ($title, 0, $i));
                 
                 //find words that should always be lowercase…
                 //(never on the first word, and never if preceded by a colon)
-                $m = $i>0 && mb_substr ($title, max (0, $i-2), 1, 'UTF-8') !== ':' && 
-                    !preg_match ('/[\x{2014}\x{2013}] ?/u', mb_substr ($title, max (0, $i-2), 2, 'UTF-8')) && 
+				$m = $i>0 && substr ($title, max (0, $i-2), 1) !== ':' && 
                     preg_match ('/^(a(nd?|s|t)?|b(ut|y)|en|for|i[fn]|o[fnr]|t(he|o)|vs?\.?|via)[ \-]/i', $m)
                 ?	//…and convert them to lowercase
-                    mb_strtolower ($m, 'UTF-8')
+					strtolower ($m)
                     
                 //else:	brackets and other wrappers
-                : (	preg_match ('/[\'"_{(\[‘“]/u', mb_substr ($title, max (0, $i-1), 3, 'UTF-8'))
+				: (	preg_match ('/[\'"_{(\[‘“]/', substr ($title, max (0, $i-1), 3))
                 ?	//convert first letter within wrapper to uppercase
-                    mb_substr ($m, 0, 1, 'UTF-8').
-                    mb_strtoupper (mb_substr ($m, 1, 1, 'UTF-8'), 'UTF-8').
-                    mb_substr ($m, 2, mb_strlen ($m, 'UTF-8')-2, 'UTF-8')
+					substr ($m, 0, 1).
+					strtoupper (substr ($m, 1, 1)).
+					substr ($m, 2, strlen ($m)-2)
                     
                 //else:	do not uppercase these cases
-                : (	preg_match ('/[\])}]/', mb_substr ($title, max (0, $i-1), 3, 'UTF-8')) ||
-                    preg_match ('/[A-Z]+|&|\w+[._]\w+/u', mb_substr ($m, 1, mb_strlen ($m, 'UTF-8')-1, 'UTF-8'))
+				: (	preg_match ('/[\])}]/', substr ($title, max (0, $i-1), 3)) ||
+					preg_match ('/[A-Z]+|&|\w+[._]\w+/', substr ($m, 1, strlen ($m)-1))
                 ?	$m
                     //if all else fails, then no more fringe-cases; uppercase the word
-                :	mb_strtoupper (mb_substr ($m, 0, 1, 'UTF-8'), 'UTF-8').
-                    mb_substr ($m, 1, mb_strlen ($m, 'UTF-8'), 'UTF-8')
+				:	strtoupper (substr ($m, 0, 1)).
+					substr ($m, 1, strlen ($m))
                 ));
                 
                 //resplice the title with the change (`substr_replace` is not multi-byte aware)
-                $title = mb_substr ($title, 0, $i, 'UTF-8').$m.
-                    mb_substr ($title, $i+mb_strlen ($m, 'UTF-8'), mb_strlen ($title, 'UTF-8'), 'UTF-8')
+				$title = substr ($title, 0, $i).$m.
+					 substr ($title, $i+strlen($m), strlen ($title))
                 ;
             }
             
             //restore the HTML
             foreach ($html[0] as &$tag) $title = substr_replace ($title, $tag[0], $tag[1], 0);
             return $title;
-
-            
 		}
 
     }
